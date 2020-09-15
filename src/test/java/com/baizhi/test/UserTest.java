d737f5ed5715ee4b72c081633c90210a237831f7
17536206291 <zhaoyu7076@163.com>
2020-09-16 01:03:57 +0800

    eeeeeeeeeeee

--- src/test/java/com/baizhi/test/UserTest.java
+++ src/test/java/com/baizhi/test/UserTest.java
@@ -9,7 +9,7 @@ public class UserTest {
     public void test(){
         User user = new User(6,"赵宇","2222",34);
         System.out.println(user);
-        System.out.println("wwwwwwww111111111111111111111");
+        System.out.println("wwwwwwww");
     }
 
 
