154a6eed1c24c8051d9cba49fbbf1bf4f6c3f66b
17536206291 <zhaoyu7076@163.com>
2020-09-16 00:58:14 +0800

    真的很好

--- src/test/java/com/baizhi/test/UserTest.java
+++ src/test/java/com/baizhi/test/UserTest.java
@@ -9,6 +9,8 @@ public class UserTest {
     public void test(){
         User user = new User(6,"赵宇","2222",34);
         System.out.println(user);
+        System.out.println("wwwwwwww");
     }
+
 
 }
